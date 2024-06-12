2e375b8f8bb1bc5f49bea9d27f2975dfb188a725
Darko <darkoshypka@gmail.com>
2024-06-12 14:28:14 +0200

    created file and added imports

---
+++ game.py
@@ -0,0 +1,11 @@
+import json
+import time
+from datetime import datetime
+from PySide6.QtWidgets import QApplication, QWidget, QPushButton, QLabel, QVBoxLayout, \
+    QHBoxLayout, QMessageBox, QInputDialog, QFileDialog, QSlider
+import threading
+from PySide6.QtGui import QPixmap, QPainter, QColor, QFont, QMouseEvent, QIcon, QPen
+from PySide6.QtCore import Qt, QTimer, QRect
+import random
+
+
