cda1bb8fbb5d9684049b0f9e607bd501803f519f
Darko <darkoshypka@gmail.com>
2024-06-12 14:43:09 +0200

    created LoginWindow class and GameWidget class

--- game.py
+++ game.py
@@ -8,4 +8,30 @@ from PySide6.QtGui import QPixmap, QPainter, QColor, QFont, QMouseEvent, QIcon, 
 from PySide6.QtCore import Qt, QTimer, QRect
 import random
 
+class LoginWindow(QWidget):
+    window_width = 1000
+    window_height = 800
 
+    CONFIG_FILE = "config.json"
+
+    def __init__(self):
+        super().__init__()
+        self.error_label = QLabel(self)
+        self.game_display = GameWidget(self)
+        self.instruction_label = QLabel(self)
+        self.new_game_button = None
+        self.save_game_button = None
+        self.load_game_button = None
+        self.exit_button = None
+        self.last_saved_filename = None
+
+
+
+
+class GameWidget(QWidget):
+    square = 3
+    dimension = square * square
+    side = LoginWindow.window_width - 400
+    cell_length = side // dimension
+    thick_line = 8
+    thin_line = 2
